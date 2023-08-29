4cee808fd3dec866d4525b681d61c8fd6bce6023
fungusflip <alfredo.momp@gmail.com>
2023-08-29 16:17:13 +0200

    PP

--- Runner/Assets/Scripts/CharacterMove.cs
+++ Runner/Assets/Scripts/CharacterMove.cs
@@ -6,17 +6,31 @@ public class CharacterMove : MonoBehaviour
 {
     private Rigidbody2D rg; // Note the corrected spelling of "Rigidbody"
 
+    [SerializeField] private Transform targetTransform;
     [SerializeField] float moveForce = 10.0f; // Adjust this value to control the force magnitude
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform cameraPos;
+    private bool Stopped = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
+        rg.velocity = targetTransform.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rg.AddForce(Vector3.right * moveForce);
+        if (rg.velocity.magnitude == 0)
+        {
+            Stopped = true;
+        }
+        Debug.Log(Stopped);
+
+        if (Stopped == true)
+        {
+            rg.add
+        }
     }
 }
